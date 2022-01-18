3db098f0954b4aedafa76faaaec1588f1a57d288
seoyeon <hongssup0202@gmail.com>
2022-01-18 16:39:35 +0900

    Date and price extraction using regex

--- ocr/MLKitOCRViewController.swift
+++ ocr/MLKitOCRViewController.swift
@@ -15,6 +15,9 @@ class MLKitOCRViewController: UIViewController {
     @IBOutlet weak var btnLibrary: UIButton!
     @IBOutlet weak var textView: UITextView!
     
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
     let picker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -52,6 +55,7 @@ class MLKitOCRViewController: UIViewController {
     func getText(_ image: UIImage) {
         let koreanOptions = KoreanTextRecognizerOptions()
         let textRecognizer = TextRecognizer.textRecognizer(options: koreanOptions)
+        
         let visionImage = VisionImage(image: image)
         visionImage.orientation = image.imageOrientation
         
@@ -62,8 +66,42 @@ class MLKitOCRViewController: UIViewController {
                 return
             }
             //recognized text
-            print(result.text)
-            self.textView.text = result.text
+            let Str = result.text
+            print(Str)
+
+            let datePattern  = "(?<year>(19|20)[0-9]{2})[-/.](?<month>(0[1-9]|1[0-2]))[-/.](?<date>(0[1-9]|1[0-9]|2[0-9]|3[0-1]))"
+            let dateRegex = try? NSRegularExpression(pattern: datePattern, options: [])
+            if let dateResult = dateRegex?.matches(in: Str, options: [], range: NSRange(location: 0, length: Str.count)) {
+                let dateStrings = dateResult.map { (element) -> String in
+                    let yearRange = Range(element.range(withName: "year"), in: Str)!
+                    let monthRange = Range(element.range(withName: "month"), in: Str)!
+                    let dateRange = Range(element.range(withName: "date"), in: Str)!
+                    return "\(Str[yearRange])년 \(Str[monthRange])월 \(Str[dateRange])일"
+                }
+                print("날짜: \(dateStrings)")
+                if !(dateStrings.isEmpty) {
+                    DispatchQueue.main.async {
+                        self.dateLabel.text = "날짜: \(dateStrings[0])"
+                    }
+                }
+            }
+            
+            let pricePattern = "([0-9]{0,3})[,.]?([0-9]{0,3})[,.]([0-9]{3})"
+            let priceRegex = try? NSRegularExpression(pattern: pricePattern, options: [])
+            if let priceResult = priceRegex?.matches(in: Str, options: [], range: NSRange(location: 0, length: Str.count)) {
+                let priceStrings = priceResult.map { (element) -> String in
+                    let range = Range(element.range, in: Str)!
+                    return String(Str[range]).replacingOccurrences(of: ",", with: "").replacingOccurrences(of: ".", with: "")
+                }
+                print("priceStrings: \(priceStrings)")
+                if !(priceStrings.isEmpty) {
+                    let priceInt = priceStrings.map { Int($0)!}
+                    print("합계: \(priceInt.max()!)")
+                    DispatchQueue.main.async {
+                        self.priceLabel.text = "합계: \(priceInt.max()!)원"
+                    }
+                }
+            }
         }
     }
     
