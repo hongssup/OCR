5394f8aecca76150dab40f8d646c17afd1e42540
seoyeon <hongssup0202@gmail.com>
2022-01-25 17:42:01 +0900

    Extract date, product and price from lines

--- ocr/ClovaOCRViewController.swift
+++ ocr/ClovaOCRViewController.swift
@@ -13,6 +13,8 @@ class ClovaOCRViewController: UIViewController {
     @IBOutlet weak var btnCamera: UIButton!
     @IBOutlet weak var btnLibrary: UIButton!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var productLabel: UILabel!
     
     let picker = UIImagePickerController()
     var textArr = [String]()
@@ -68,6 +70,9 @@ class ClovaOCRViewController: UIViewController {
                     let data = try decoder.decode(ClovaOCRModel.self, from:str)
                     if data != nil {
                         let num = data.images[0].fields.count
+                        self.textArr = []
+                        self.verticeArr = [[Float]]()
+                        self.linesArr = [[String]]()
                         for i in 0...num-1 {
                             let text = data.images[0].fields[i].inferText
                             let y = data.images[0].fields[i].boundingPoly.vertices[0].y
@@ -100,6 +105,102 @@ class ClovaOCRViewController: UIViewController {
             }
         }
         print(linesArr)
+        dateLabel.text = ""
+        productLabel.text = ""
+        for i in 0...linesArr.count-2 {
+            if linesArr[i].contains("수량") {
+                if priceCheck(linesArr[i+1].last!) {
+                    for j in (i+1)...linesArr.count-1 {
+                        if koCheck(linesArr[j][0]) {
+                            print("상품: \(linesArr[j].first!), 가격: \(linesArr[j].last!)원")
+                            DispatchQueue.main.async {
+                                self.productLabel.text = (self.productLabel.text ?? "") + "상품: \(self.linesArr[j].first!), 가격: \(self.linesArr[j].last!)원\n"
+                            }
+                        } else {
+                            print("상품: \(linesArr[j][1]), 가격: \(linesArr[j].last!)원")
+                            DispatchQueue.main.async {
+                                self.productLabel.text = (self.productLabel.text ?? "") + "상품: \(self.linesArr[j][1]), 가격: \(self.linesArr[j].last!)원\n"
+                            }
+                        }
+                        if (linesArr[j+1].joined().contains("금액")) || linesArr[j+1].first!.contains("공급가") || linesArr[j+1].first!.contains("부가세") || linesArr[j+1].first!.contains("합계") {
+                            break
+                        }
+                    }
+                } else if priceCheck(linesArr[i+2].last!) {
+                    for j in stride(from: i+1, through: linesArr.count-1, by: 2) {
+                        if koCheck(linesArr[j][0]) {
+                            print("상품: \(linesArr[j][0]), 가격: \(linesArr[j+1].last!)원")
+                            DispatchQueue.main.async {
+                                self.productLabel.text = (self.productLabel.text ?? "") + "상품: \(self.linesArr[j][0]), 가격: \(self.linesArr[j+1].last!)원\n"
+                            }
+                        } else {
+                            print("상품: \(linesArr[j][1]), 가격: \(linesArr[j+1].last!)원")
+                            DispatchQueue.main.async {
+                                self.productLabel.text = (self.productLabel.text ?? "") + "상품: \(self.linesArr[j][1]), 가격: \(self.linesArr[j+1].last!)원\n"
+                            }
+                        }
+                        if (linesArr[j+2].joined().contains("금액")) || linesArr[j+2].first!.contains("공급가") || linesArr[j+2].first!.contains("부가세") || linesArr[j+2].first!.contains("합계") {
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        for i in linesArr {
+            let date = dateCheck(i.joined())
+            if !(date.isEmpty) {
+                print(date)
+                DispatchQueue.main.async {
+                    self.dateLabel.text = "날짜: \(date)"
+                }
+                break
+            }
+        }
+    }
+    
+    func koCheck(_ text: String) -> Bool {
+        let koPattern = "[가-힣]"
+        let koRegex = try? NSRegularExpression(pattern: koPattern, options: [])
+        if let koResult = koRegex?.matches(in: text, options: [], range: NSRange(location: 0, length: 1)) {
+            let koStrings = koResult.map { (element) -> String in
+                return text
+            }
+            if !(koStrings.isEmpty) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func priceCheck(_ text: String) -> Bool {
+        let pricePattern = "([0-9]{0,3})[,.]?([0-9]{0,3})[,.]([0-9]{3})"
+        let priceRegex = try? NSRegularExpression(pattern: pricePattern, options: [])
+        if let priceResult = priceRegex?.matches(in: text, options: [], range: NSRange(location: 0, length: text.count)) {
+            let priceStrings = priceResult.map { (element) -> String in
+                return text
+            }
+            if !(priceStrings.isEmpty) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func dateCheck(_ text: String) -> String {
+        let datePattern  = "(?<year>(19|20)[0-9]{2})[-/.](?<month>(0[1-9]|1[0-2]))[-/.](?<date>(0[1-9]|1[0-9]|2[0-9]|3[0-1]))"
+        let dateRegex = try? NSRegularExpression(pattern: datePattern, options: [])
+        if let dateResult = dateRegex?.matches(in: text, options: [], range: NSRange(location: 0, length: text.count)) {
+            let dateStrings = dateResult.map { (element) -> String in
+                let yearRange = Range(element.range(withName: "year"), in: text)!
+                let monthRange = Range(element.range(withName: "month"), in: text)!
+                let dateRange = Range(element.range(withName: "date"), in: text)!
+                return "\(text[yearRange])년 \(text[monthRange])월 \(text[dateRange])일"
+            }
+            if !(dateStrings.isEmpty) {
+                return dateStrings[0]
+            }
+        }
+        return ""
     }
 }
 
